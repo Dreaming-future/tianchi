5c312062af92fee31e6aaec14dfda1e5be98fd35
Dreaming-future <1016617094@qq.com>
2022-03-27 19:37:42 +0800

    街景识别所需要的数据集文件

---
+++ cv-CharacterCodingRecognition/dataset/get_test.sh
@@ -0,0 +1,3 @@
+wget http://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/531795/mchar_test_a.zip
+unzip -q mchar_test_a.zip
+wget http://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/531795/mchar_sample_submit_A.csv
---
+++ cv-CharacterCodingRecognition/dataset/get_train.sh
@@ -0,0 +1,11 @@
+# Download data.
+wget http://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/531795/mchar_train.json
+wget http://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/531795/mchar_val.json
+
+wget http://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/531795/mchar_train.zip
+unzip -o -q mchar_train.zip
+
+wget http://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/531795/mchar_val.zip
+unzip -o -q mchar_val.zip
+
+
